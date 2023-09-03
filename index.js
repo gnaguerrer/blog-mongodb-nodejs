c899e247c347d454ea499c076b966119382f73b5
Gina Guerrero <gnaguerrer@gmail.com>
2023-09-03 14:11:41 -0500

    feat: node server CORS setup and json parser

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const express = require("express");
+const cors = require("cors");
 const { connection } = require("./database/connection");
+
+const PORT = 3900;
+const app = express();
+
+// Setup CORS
+app.use(cors());
+
+// Parse body to json
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("Server running at PORT", PORT);
+});
 
 connection();
