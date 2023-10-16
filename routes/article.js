5e43a7048f464631f44e2cae82bc133f51c054fc
Gina Guerrero <gnaguerrer@gmail.com>
2023-10-15 19:59:47 -0500

    feat: initial multer config

--- routes/article.js
+++ routes/article.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
 const ArticleContoller = require("../controllers/articleController");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images/articles/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "artcile" + Date.now() + file.originalname);
+  },
+});
+
+const uploadedImages = multer({ storage });
 
 router.post("/article", ArticleContoller.createArticle);
 router.get("/article", ArticleContoller.getArticles);
 router.get("/article/:articleId", ArticleContoller.getArticleById);
 router.delete("/article/:articleId", ArticleContoller.deleteArticleById);
 router.put("/article/:articleId", ArticleContoller.updateArticle);
+router.post(
+  "/article_image/:articleId",
+  [uploadedImages.single("file")],
+  ArticleContoller.uplaodFile
+);
 
 module.exports = router;
